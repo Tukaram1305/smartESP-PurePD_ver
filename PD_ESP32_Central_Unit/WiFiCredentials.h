dc7b4f85af3a4bf9815e0557b8f8a17e8cd9c821
Markowiak Paweł <93986877+Tukaram1305@users.noreply.github.com>
2024-01-04 13:44:14 +0100

    Update WiFiCredentials.h

--- PD_ESP32_Central_Unit/WiFiCredentials.h
+++ PD_ESP32_Central_Unit/WiFiCredentials.h
@@ -12,7 +12,7 @@ IPAddress secondaryDNS(8, 8, 4, 4);
 
 //  Dane uwierzytelnienia kanalu na serwerze ThingSpeak
 unsigned long myChannelNumber = 1896531;
-const char * myWriteAPIKey = "PYZHTWT1I6E9BZLY";
+const char * myWriteAPIKey = "KEY";
 
 //  Adres lokalnego serwera WWW na maszynie stacjonarnej
 String ServerAddress = "192.168.1.55/smartesp";
